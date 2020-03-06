053a06b296cc092a6eff35196015eb90412a69a9
mikebin170 <m18321807715@163>
2020-03-06 21:55:16 +0800

    yaml数据驱动测试

--- unit/testYaml/test_yaml.py
+++ unit/testYaml/test_yaml.py
@@ -2,5 +2,28 @@ import yaml
 
 
 class TestYaml:
+    #一维数组
+    # - Hesperiidae
+    # - Papilionidae
+    # - Apatelodidae
+    # - Epiplemidae
+    #输出：['Hesperiidae', 'Papilionidae', 'Apatelodidae', 'Epiplemidae']
+    #如下是二维数组
     def test_yaml(self):
-        yaml.load()
+        print(yaml.load("""
+    -
+        - 1
+        - 2
+        - 3
+    -
+        - 4
+        - 5
+        - 6   
+        """))
+
+    # @pytest.mark.parametrize("key, stock_type, price", [
+    #     ("alibaba", "BABA", 200),
+    #     ("JD", "JD", 20)
+    # ])
+    def test_yaml_zhidian(self):
+        print(yaml.safe_load(open("test.yaml")))
